5939a69de688715e660f83732b772218887caf1f
AbuHoney <abdalrahman_mohd@yahoo.com>
2022-11-09 05:15:45 +0300

    Last Update

---
+++ script.js
@@ -0,0 +1,98 @@
+'use strict';
+
+// General
+document.querySelector('.player--active');
+document.querySelector('.btn--new').addEventListener('click', resetGame);
+document.querySelector('.btn--roll').addEventListener('click', rollClick);
+document.querySelector('.btn--hold').addEventListener('click', holdClick);
+var diceImg = document.querySelector('.dice');
+diceImg.classList.add('hidden');
+// player--1
+var playerOne = document.querySelector('.player--0');
+var scorePlayerOne = document.querySelector('#score--0');
+var currentPlayerOne = document.querySelector('#current--0');
+// player--2
+var playerTwo = document.querySelector('.player--1');
+var scorePlayerTwo = document.querySelector('#score--1');
+var currentPlayerTwo = document.querySelector('#current--1');
+
+// start coding
+function resetGame() {
+  currentPlayerOne.textContent = 0;
+  scorePlayerOne.textContent = 0;
+  currentPlayerTwo.textContent = 0;
+  scorePlayerTwo.textContent = 0;
+
+  if (!playerOne.classList.contains('player--active')) {
+    playerOne.classList.add('player--active');
+    playerTwo.classList.remove('player--active');
+  }
+
+  if (!diceImg.classList.contains('hidden')) diceImg.classList.add('hidden');
+}
+resetGame();
+function rollClick() {
+  if (diceImg.classList.contains('hidden')) diceImg.classList.remove('hidden');
+  var rollDice = Math.trunc(Math.random() * 6 + 1);
+  if (playerOne.classList.contains('player--active')) {
+    diceImg.src = `dice-${rollDice}.png`;
+    // player 1
+    if (rollDice === 1) {
+      currentPlayerOne.textContent = 0;
+      holdClick();
+    } else {
+      var total = Number(currentPlayerOne.textContent) + rollDice;
+      currentPlayerOne.textContent = total;
+    }
+  } else {
+    // player 2
+    diceImg.src = `dice-${rollDice}.png`;
+    if (rollDice === 1) {
+      currentPlayerTwo.textContent = 0;
+      holdClick();
+    } else {
+      var total = Number(currentPlayerTwo.textContent) + rollDice;
+      currentPlayerTwo.textContent = total;
+    }
+  }
+}
+
+function holdClick() {
+  if (playerOne.classList.contains('player--active')) {
+    var finalScore1 =
+      Number(scorePlayerOne.textContent) + Number(currentPlayerOne.textContent);
+
+    scorePlayerOne.textContent = finalScore1;
+    if (Number(scorePlayerOne.textContent) >= 100) {
+      disabledButton();
+      playerOne.style.backgroundColor = '#2f2f2f';
+      diceImg.classList.add('hidden');
+    } else {
+      currentPlayerOne.textContent = 0;
+      playerOne.classList.remove('player--active');
+      playerTwo.classList.add('player--active');
+    }
+  } else {
+    var finalScore2 =
+      Number(scorePlayerTwo.textContent) + Number(currentPlayerTwo.textContent);
+    scorePlayerTwo.textContent = finalScore2;
+    if (Number(scorePlayerTwo.textContent) >= 100) {
+      disabledButton();
+      playerOne.style.backgroundColor = '#2f2f2f';
+      diceImg.classList.add('hidden');
+    } else {
+      currentPlayerTwo.textContent = 0;
+      playerOne.classList.add('player--active');
+      playerTwo.classList.remove('player--active');
+    }
+  }
+}
+
+function disabledButton() {
+  document.querySelector('.btn--roll').disabled = true;
+  document.querySelector('.btn--hold').disabled = true;
+}
+
+// classList.toggle() check if class exsist remove it and if not exist add it
+// playerTwo.classList.toggle("player--active"); check on playerOne
+// playerOne.classList.toggle('player--active');
